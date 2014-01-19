8df803d6f43ccb2194ea03d5b3303381fe2ee934
Timothy King <tmk@lordzork.com>
2014-01-19 12:32:31 -0500

    Adds mobile interface

--- content/assets/javascripts/app.js
+++ content/assets/javascripts/app.js
@@ -1,3 +1,9 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-//$(document).foundation();
+$(document).ready(function() {
+  $(document).foundation();
+
+  $(".off-canvas-list a").on("click.toggleCanvas", function(){
+    $(".exit-off-canvas").click();
+  });
+});
