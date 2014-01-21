79c563ef3d4b782cd953206f361107ed09738fd3
Timothy King <tmk@lordzork.com>
2014-01-21 15:58:15 -0500

    Fixed bug with the topbar button

--- content/assets/javascripts/app.js
+++ content/assets/javascripts/app.js
@@ -1,7 +1,6 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).ready(function() {
-  $(document).foundation();
 
   $(".off-canvas-list a").on("click.toggleCanvas", function(){
     $(".exit-off-canvas").click();
